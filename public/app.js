b1fbe1e9eeccb9310f14f3ed49fc45bcc747ed78
Francis Chong <francis@ignition.hk>
2016-03-04 01:14:21 +0800

    extract the script to separate file

--- public/app.js
+++ public/app.js
@@ -1,10 +1,22 @@
-var vid = document.getElementById("bgvid");
+var myDataRef = new Firebase('https://atv-rip.firebaseio.com/messages');
 
-function vidFade() {
-  vid.classList.add("stopfade");
+myDataRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+})
+
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messages'));
+  $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
 }
 
-vid.addEventListener('ended', function () {
-  vid.pause();
-  vidFade();
+$('#message').keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#name').val()
+    var text = $('#message').val()
+    if (name.length > 0 && text.length > 0) {
+      myDataRef.push({name: name, text: text})
+      $('#messageInput').val('')
+    }
+  }
 })
