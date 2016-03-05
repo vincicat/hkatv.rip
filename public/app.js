6a312cf9bc4fc2bab86c9a0662a9ef5a25dfcaa9
Kelvin Wong <netsgnut@gmail.com>
2016-03-06 02:47:56 +0800

    Wrapped app.js in a closure

--- public/app.js
+++ public/app.js
@@ -1,50 +1,57 @@
-var isMobile = (window.matchMedia('(max-device-width: 800px)').matches);
+(function () {
+  var isMobile = (window.matchMedia('(max-device-width: 800px)').matches);
 
-var myDataRef = new Firebase('https://atv-rip.firebaseio.com/messages');
+  // Queue messages
+  var pendingMessages = [];
 
-var pendingMessages = [];
+  var dataRef = new Firebase('https://atv-rip.firebaseio.com/messages');
+  var lastMessageQuery = dataRef.limitToLast(isMobile ? 25 : 100);
+  lastMessageQuery.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    pendingMessages.push(message.text);
+  });
 
-var lastMessageQuery = myDataRef.limitToLast(isMobile ? 25 : 100);
-lastMessageQuery.on('child_added', function(snapshot) {
-  var message = snapshot.val();
-  pendingMessages.push(message.text);
-});
+  // Display message as marquee
+  var marqueeHolder = $('#messages')[0];
 
-setInterval(function () {
-  var message = pendingMessages.shift();
-  if (message) {
-    displayChatMessage(message);
-  }
-}, 1000);
-
-var marqueeHolder = $('#messages')[0];
-
-function displayChatMessage(text) {
-  var marqueeDiv = document.createElement('div');
-  marqueeDiv.className = 'marquee';
-  marqueeDiv.style.top = Math.floor(Math.random() * 90) + '%';
-  marqueeDiv.style.fontSize = Math.floor(Math.random() * 4 + 3) + 'vmin';
-  if (!isMobile) {
-    marqueeDiv.style.animationDelay = Math.floor(Math.random() * 8) + 's';
-    marqueeDiv.style.animationDuration = 15 + Math.floor(Math.random() * 4) * 3 + 's';
-  }
-  marqueeDiv.textContent = text;
-  marqueeHolder.appendChild(marqueeDiv);
-}
-
-$('#name').val(localStorage.getItem('name'));
-$('#name').on('input propertychange paste', function (e) {
-  localStorage.setItem('name', $(this).val());
-});
-
-$('#message').on('keydown', function (e) {
-  if (e.keyCode == 13) {
-    var name = $('#name').val();
-    var text = $('#message').val();
-    if (name.length > 0 && text.length > 0) {
-      myDataRef.push({ name: name, text: text });
-      $('#message').val('');
+  function displayChatMessage(text) {
+    var marqueeDiv = document.createElement('div');
+    marqueeDiv.className = 'marquee';
+    marqueeDiv.style.top = Math.floor(Math.random() * 90) + '%';
+    marqueeDiv.style.fontSize = Math.floor(Math.random() * 4 + 3) + 'vmin';
+    if (!isMobile) {
+      marqueeDiv.style.animationDelay = Math.floor(Math.random() * 8) + 's';
+      marqueeDiv.style.animationDuration = 15 + Math.floor(Math.random() * 4) * 3 + 's';
     }
+    marqueeDiv.textContent = text;
+    marqueeHolder.appendChild(marqueeDiv);
   }
-});
+
+  setInterval(function () {
+    var message = pendingMessages.shift();
+    if (message) {
+      displayChatMessage(message);
+    }
+  }, 1000);
+
+  // Form
+  var $nameEl = $('#name');
+  var $messageEl = $('#message');
+
+  $nameEl.val(localStorage.getItem('name'));
+  $nameEl.on('input propertychange paste', function (e) {
+    localStorage.setItem('name', $nameEl.val());
+  });
+
+  $messageEl.on('keydown', function (e) {
+    if (e.keyCode == 13) {
+      var name = $nameEl.val();
+      var text = $messageEl.val();
+      if (name.length > 0 && text.length > 0) {
+        dataRef.push({ name: name, text: text });
+        $messageEl.val('');
+      }
+    }
+  });
+})();
 
