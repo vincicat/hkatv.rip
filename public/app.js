97b2ac2f16403c4ebb50147113921f67a6288180
Kelvin Wong <netsgnut@gmail.com>
2016-03-06 02:40:46 +0800

    Made mobile experience a bit more bearable

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
+var isMobile = (window.matchMedia('(max-device-width: 800px)').matches);
+
 var myDataRef = new Firebase('https://atv-rip.firebaseio.com/messages');
 
 var pendingMessages = [];
 
-var lastMessageQuery = myDataRef.limitToLast(100);
+var lastMessageQuery = myDataRef.limitToLast(isMobile ? 25 : 100);
 lastMessageQuery.on('child_added', function(snapshot) {
   var message = snapshot.val();
   pendingMessages.push(message.text);
@@ -21,10 +23,12 @@ function displayChatMessage(text) {
   var marqueeDiv = document.createElement('div');
   marqueeDiv.className = 'marquee';
   marqueeDiv.style.top = Math.floor(Math.random() * 90) + '%';
-  marqueeDiv.style.fontSize = Math.floor(Math.random() * 7 + 20) + 'pt';
-  marqueeDiv.style.animationDelay = Math.floor(Math.random() * 8) + 's';
-  marqueeDiv.style.animationDuration = 15 + Math.floor(Math.random() * 4) * 3 + 's';
-  marqueeDiv.innerHTML = text;
+  marqueeDiv.style.fontSize = Math.floor(Math.random() * 4 + 3) + 'vmin';
+  if (!isMobile) {
+    marqueeDiv.style.animationDelay = Math.floor(Math.random() * 8) + 's';
+    marqueeDiv.style.animationDuration = 15 + Math.floor(Math.random() * 4) * 3 + 's';
+  }
+  marqueeDiv.textContent = text;
   marqueeHolder.appendChild(marqueeDiv);
 }
 
